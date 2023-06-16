a2ccdc6da4c43abd152f8b7f2f373ed8f6622947
Albert Felix <albertfelixleo@gmail.com>
2023-06-17 04:37:52 +0530

    added jadenCase

---
+++ jadenCase.js
@@ -0,0 +1,8 @@
+String.prototype.toJadenCase = function () {
+  let splitStr = this.toLowerCase().split(" ");
+  for (let i = 0; i < splitStr.length; i++) {
+    splitStr[i] =
+      splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+  }
+  return splitStr.join(" ");
+};
